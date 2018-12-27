2cec21f6435b6e435dce536218b5cd44d0d21156
zhaopengproject <273615813@qq.com>
2018-12-27 17:01:51 +0800

    zhao

---
+++ core-common/src/main/java/org/zhao/core/common/util/SignUtil.java
@@ -0,0 +1,259 @@
+package org.zhao.core.common.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * 身份信息
+ * @author zhao
+ *
+ */
+public class SignUtil {
+	
+	private  final static String KEY_ALGORITHM = "AES";
+    private  final static String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
+	
+    public  final static String CHARSET = "UTF-8";
+    public  final static String RSA_ALGORITHM = "RSA";
+    
+	/**
+	 * 对请求的内容做sing处理获得请求id
+	 * @param parameterJson 请求参数 map转json
+	 * @param times 时间戳
+	 */
+	public static String getHttpContext(String parameterJson) {
+		String signStr = ConfigProperties.instance().getPropertiesVal("sign.http.key") + "<&>" + parameterJson;
+		signStr = encrypt(signStr, ConfigProperties.instance().getPropertiesVal("sign.http.aes.key")) + "<*>" + System.currentTimeMillis();
+		try {
+			signStr = publicEncrypt(signStr, getPublicKey(ConfigProperties.instance().getPropertiesVal("sign.http.rsa.public.key")));
+			return ZipUtil.zip(signStr);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	
+	/**
+     * AES 加密操作
+     *
+     * @param content 待加密内容
+     * @param password 加密密码
+     * @return 返回Base64转码后的加密数据
+     */
+    private static String encrypt(String content, String password) {
+        try {
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);// 创建密码器
+
+            byte[] byteContent = content.getBytes("utf-8");
+
+            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(password));// 初始化为加密模式的密码器
+
+            byte[] result = cipher.doFinal(byteContent);// 加密
+
+            return Base64.encodeBase64String(result);//通过Base64转码返回
+        } catch (Exception ex) {
+        }
+
+        return null;
+    }
+    
+    /**
+     * 生成加密秘钥
+     *
+     * @return
+     */
+    private static SecretKeySpec getSecretKey(final String password) {
+        //返回生成指定算法密钥生成器的 KeyGenerator 对象
+        KeyGenerator kg = null;
+
+        try {
+            kg = KeyGenerator.getInstance(KEY_ALGORITHM);
+
+            //AES 要求密钥长度为 128
+            kg.init(128, new SecureRandom(password.getBytes()));
+
+            //生成一个密钥
+            SecretKey secretKey = kg.generateKey();
+
+            return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);// 转换为AES专用密钥
+        } catch (NoSuchAlgorithmException ex) {
+        	
+        }
+
+        return null;
+    }
+    
+    /**
+     * AES 解密操作
+     *
+     * @param content
+     * @param password
+     * @return
+     */
+    private static String decrypt(String content, String password) {
+
+        try {
+            //实例化
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+
+            //使用密钥初始化，设置为解密模式
+            cipher.init(Cipher.DECRYPT_MODE, getSecretKey(password));
+
+            //执行操作
+            byte[] result = cipher.doFinal(Base64.decodeBase64(content));
+
+            return new String(result, "utf-8");
+        } catch (Exception ex) {
+        }
+        
+        return null;
+    }
+    
+    /**
+     * 公钥加密
+     * @param data
+     * @param publicKey
+     * @return
+     */
+    private  static String publicEncrypt(String data, RSAPublicKey publicKey){
+        try{
+            Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            return Base64.encodeBase64URLSafeString(rsaSplitCodec(cipher, Cipher.ENCRYPT_MODE, data.getBytes(CHARSET), publicKey.getModulus().bitLength()));
+        }catch(Exception e){
+            throw new RuntimeException("加密字符串[" + data + "]时遇到异常", e);
+        }
+    }
+    
+    /**
+     * 私钥解密
+     * @param data
+     * @param privateKey
+     * @return
+     */
+    private  static String privateDecrypt(String data, RSAPrivateKey privateKey){
+        try{
+            Cipher cipher = Cipher.getInstance(RSA_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            return new String(rsaSplitCodec(cipher, Cipher.DECRYPT_MODE, Base64.decodeBase64(data), privateKey.getModulus().bitLength()), CHARSET);
+        }catch(Exception e){
+            throw new RuntimeException("解密字符串[" + data + "]时遇到异常", e);
+        }
+    }
+    
+    private  static Map<String, String> createKeys(int keySize){
+        //为RSA算法创建一个KeyPairGenerator对象
+        KeyPairGenerator kpg;
+        try{
+            kpg = KeyPairGenerator.getInstance(RSA_ALGORITHM);
+        }catch(NoSuchAlgorithmException e){
+            throw new IllegalArgumentException("No such algorithm-->[" + RSA_ALGORITHM + "]");
+        }
+
+        //初始化KeyPairGenerator对象,密钥长度
+        kpg.initialize(keySize);
+        //生成密匙对
+        KeyPair keyPair = kpg.generateKeyPair();
+        //得到公钥
+        Key publicKey = keyPair.getPublic();
+        String publicKeyStr = Base64.encodeBase64URLSafeString(publicKey.getEncoded());
+        //得到私钥
+        Key privateKey = keyPair.getPrivate();
+        String privateKeyStr = Base64.encodeBase64URLSafeString(privateKey.getEncoded());
+        Map<String, String> keyPairMap = new HashMap<String, String>();
+        keyPairMap.put("publicKey", publicKeyStr);
+        keyPairMap.put("privateKey", privateKeyStr);
+        return keyPairMap;
+    }
+    
+    /**
+     * 得到公钥
+     * @param publicKey 密钥字符串（经过base64编码）
+     * @throws Exception
+     */
+    private  static RSAPublicKey getPublicKey(String publicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        //通过X509编码的Key指令获得公钥对象
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(Base64.decodeBase64(publicKey));
+        RSAPublicKey key = (RSAPublicKey) keyFactory.generatePublic(x509KeySpec);
+        return key;
+    }
+
+    /**
+     * 得到私钥
+     * @param privateKey 密钥字符串（经过base64编码）
+     * @throws Exception
+     */
+    private  static RSAPrivateKey getPrivateKey(String privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        //通过PKCS#8编码的Key指令获得私钥对象
+        KeyFactory keyFactory = KeyFactory.getInstance(RSA_ALGORITHM);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(privateKey));
+        RSAPrivateKey key = (RSAPrivateKey) keyFactory.generatePrivate(pkcs8KeySpec);
+        return key;
+    }
+    
+    private  static byte[] rsaSplitCodec(Cipher cipher, int opmode, byte[] datas, int keySize){
+        int maxBlock = 0;
+        if(opmode == Cipher.DECRYPT_MODE){
+            maxBlock = keySize / 8;
+        }else{
+            maxBlock = keySize / 8 - 11;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] buff;
+        int i = 0;
+        try{
+            while(datas.length > offSet){
+                if(datas.length-offSet > maxBlock){
+                    buff = cipher.doFinal(datas, offSet, maxBlock);
+                }else{
+                    buff = cipher.doFinal(datas, offSet, datas.length-offSet);
+                }
+                out.write(buff, 0, buff.length);
+                i++;
+                offSet = i * maxBlock;
+            }
+        }catch(Exception e){
+            throw new RuntimeException("加解密阀值为["+maxBlock+"]的数据时发生异常", e);
+        }
+        byte[] resultDatas = out.toByteArray();
+        try {
+            if (out != null) {
+            	out.close();
+            }
+        } catch (final IOException ioe) {
+            // ignore
+        }
+        return resultDatas;
+    }
+    
+    @SuppressWarnings("restriction")
+	public static String strToBase64(String key) {
+    	return new sun.misc.BASE64Encoder().encode(key.getBytes());
+    }
+}
